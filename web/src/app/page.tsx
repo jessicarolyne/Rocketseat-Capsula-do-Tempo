80b17e575b9e066d222ce38ceb1db76f8314ab4a
Jessica <jessicarolyne@hotmail.com>
2023-06-29 13:30:54 -0300

    Inicio frontend web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-sans font-bold">Sua capsula do tempo</h1>
-      <h2 className="text-4xl font-alt font-bold">Sua capsula do tempo</h2>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+{/*Left*/}
+<div></div>
+{/*Right*/}
+<div>
+  <p>
+  Você ainda não registrou nenhuma lembrança, comece a {''}
+  <a href="">criar agora</a>!
+  </p>
+</div>
+    </main>
   )
 }
