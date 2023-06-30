de0ba265dc342994df3d47a2c395819012fd620a
Jessica <jessicarolyne@hotmail.com>
2023-06-29 22:32:27 -0300

    Inicio desenvolvimento frontend tela inicial - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import {
+  BaiJamjuree_400Regular,
+  BaiJamjuree_700Bold,
+} from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_400Regular,
+    BaiJamjuree_700Bold,
+  })
+  if(hasLoadedFonts) {
+    return null
+  }
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-5xl font-title font-bold text-gray-50">Hello World</Text>
       <StatusBar style="light" />
     </View>
   )
