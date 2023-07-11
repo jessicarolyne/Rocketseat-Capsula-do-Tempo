9a0d0646a6b819e2cc343248e3cf1cf1ff4a9785
Jessica <jessicarolyne@hotmail.com>
2023-07-11 13:33:56 -0300

    Desenvolvimento memories/new mobile e backend uploads

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,8 +1,8 @@
-import { View, Text, Touchable, Switch } from "react-native"
+import { View, Text, Touchable, Switch, TextInput } from "react-native"
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { Link } from "expo-router"
 import Icon from '@expo/vector-icons/Feather'
-import { TouchableOpacity } from "react-native-gesture-handler"
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { useState } from "react"
 
@@ -10,8 +10,8 @@ export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
   const [ isPublic, setIsPublic ] = useState(false)
   return (
-    <View className=" flex-1 px-8" 
-    style={{ paddingBottom: bottom, paddingTop: top }}>
+    <ScrollView className=" flex-1 px-8" 
+    contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}>
       <View className="mt-4 flex-row items-center justify-between">
         <NLWLogo/>
         <Link href="/memories" asChild>
@@ -31,12 +31,29 @@ export default function NewMemory() {
           </Text>
         </View>
       </View>
-      <TouchableOpacity className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
+      <TouchableOpacity 
+      activeOpacity={0.7}
+      className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
         <View className="flex-row items-center gap-2">
           <Icon name="image" color="#FFF" />
           <Text className="font-body text-sm text-gray-200">Adicionar foto ou vídeo de capa</Text>
         </View>
       </TouchableOpacity>
-    </View>
+
+      <TouchableOpacity>
+        <TextInput multiline
+        className="my-4 p-0 font-body text-lg text-gray-50"
+        placeholderTextColor="#56565a"
+        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."/>
+      </TouchableOpacity>
+      <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full items-center self-end bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Salvar
+          </Text>
+        </TouchableOpacity>
+    </ScrollView>
   )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,16 +4,25 @@ import fastify from 'fastify'
 import { memoriesRoute } from './routes/memories'
 import cors from '@fastify/cors'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
 app.register(cors, {
   origin: true,
+})
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
 })
 app.register(jwt, {
   secret: 'spacetime',
 })
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoute)
 app
   .listen({
